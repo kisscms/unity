ba15e7427336b3f3812d95d4eaf0dba6e8d85d0f
Makis Tracend <makis.tracend@gmail.com>
2012-08-12 05:43:51 -0700

    Updating name of require-path

--- views/foot.php
+++ views/foot.php
@@ -1,2 +1,2 @@
-<script type="text/javascript" src="<?=url("/js/libs/jquery.unity3d.min.js")?>" data-type="require" data-path="jquery.unity3d.min" data-deps="jquery"></script>
+<script type="text/javascript" src="<?=url("/js/libs/jquery.unity3d.min.js")?>" data-type="require" data-path="jquery.unity3d" data-deps="jquery"></script>
 
